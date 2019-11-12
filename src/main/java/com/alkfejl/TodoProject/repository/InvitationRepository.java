84b0e8f2015ae0f7063d5c47b6be6aefeabbf398
sdnmsg <sdnmsg@inf.elte.hu>
2019-11-12 14:56:01 +0100

    InvitationRepoitory and Invitation table added

---
+++ src/main/java/com/alkfejl/TodoProject/repository/InvitationRepository.java
@@ -0,0 +1,18 @@
+package com.alkfejl.TodoProject.repository;
+
+import com.alkfejl.TodoProject.model.Invitation;
+import com.alkfejl.TodoProject.model.Task;
+import com.alkfejl.TodoProject.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface InvitationRepository extends CrudRepository<Invitation, Integer> {
+    //Optional<Invitation> findAllByInvitationCode(Integer id);
+
+    //List<Invitation> findAll();
+
+}
