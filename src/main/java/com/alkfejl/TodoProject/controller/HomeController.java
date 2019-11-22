8d46c4020c176405de9f0ecdc3235f0a8272bfc9
peterlacza <lcz.peti@gmail.com>
2019-11-22 18:40:04 +0100

    Configure @CrossOrigin

--- src/main/java/com/alkfejl/TodoProject/controller/HomeController.java
+++ src/main/java/com/alkfejl/TodoProject/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.alkfejl.TodoProject.controller;
 
 import com.alkfejl.TodoProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class HomeController {
